f9a8694e4ded89d3c171388f14b32a72a1e5d366
Vitaliy Baum <vitaliy.baum@yandex.ru>
2025-05-11 06:17:49 +0700

    eslint,prettier

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,23 @@ import Image from 'next/image';
 
 export const metadata = {
   title: 'Next.js FSD Starter',
-  description: 'A simple starter template for building modular Next.js apps with App Router and FSD.',
+  description:
+    'A simple starter template for building modular Next.js apps with App Router and FSD.',
 };
 
 export default function HomePage() {
   return (
-      <main className={styles.home}>
-        <div className={styles.container}>
-          <Image src="/favicon.ico" alt="Logo" width={64} height={64} />
-          <h1>Welcome to Next.js + FSD Starter</h1>
-          <p>
-            This project is powered by <strong>App Router</strong>, <strong>TypeScript</strong>,{' '}
-            <strong>ESLint</strong>, <strong>Prettier</strong>, <strong>Stylelint</strong>, and follows
-            the <strong>Feature-Sliced Design</strong> methodology.
-          </p>
-          <code className={styles.path}>src/features/hello-world</code>
-        </div>
-      </main>
+    <main className={styles.home}>
+      <div className={styles.container}>
+        <Image src="/favicon.ico" alt="Logo" width={64} height={64} />
+        <h1>Welcome to Next.js + FSD Starter</h1>
+        <p>
+          This project is powered by <strong>App Router</strong>, <strong>TypeScript</strong>,{' '}
+          <strong>ESLint</strong>, <strong>Prettier</strong>, <strong>Stylelint</strong>, and
+          follows the <strong>Feature-Sliced Design</strong> methodology.
+        </p>
+        <code className={styles.path}>src/features/hello-world</code>
+      </div>
+    </main>
   );
 }
